e102d9ecbb8806fbabc7a5a91abb6728a2baf491
tmsantanna <71285512+tmsantanna@users.noreply.github.com>
2021-04-30 20:57:57 -0300

    Update Jogador.java

--- model/Jogador.java
+++ model/Jogador.java
@@ -51,4 +51,44 @@ class Jogador {
 			hand.clear();//Clear
 			return;
 		}
+		
+		public int caclHand() {//Calcula o melhor valor da mão considerando o valor do Às
+			int temp, resultado, resultadoB;
+			List <Carta> save = new ArrayList<Carta>();
+			
+			
+			resultado = 0;
+			for (Carta carta: hand) {//Passa por cada carta da mao
+				temp = carta.pegaValor();
+				if (temp == 1) {//Verifica se a carta é um Às
+					save.add(carta);//guarda o Às para ser calculado mais tarde
+				}
+				else {
+					resultado += temp;
+				}
+			}
+			
+			if (!save.isEmpty()) {
+				resultadoB = resultado;//Resultado alternativo é carregado
+			
+				for (Carta carta: save) {//Resolve os Àses					
+					if (11 + resultado <= 21) {
+						resultado += 11;
+					} else {
+						resultado += 1;
+					}
+				}
+				
+				if (resultado > 21) {//Verifica se deu mais de 21 para dar uma segunda chance
+					for (Carta carta: save) {//Resolve os Àses (Alternativo)
+						resultadoB +=1;
+					}
+					if(resultadoB < resultado) {//Checa se depois de passar de 21 como o Resultado normal, se o resultado B é melhor
+						resultado = resultadoB;
+					}
+				}
+				
+			}
+			return resultado;	
+		}
 }
