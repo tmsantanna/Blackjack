465d8a30dd83c96ecaf5ef501b362239c20884fd
tmsantanna <71285512+tmsantanna@users.noreply.github.com>
2021-04-30 17:40:46 -0300

    Update Jogador.java

--- Jogador.java
+++ Jogador.java
@@ -1,8 +1,9 @@
+import java.util.*;
 
 class Jogador {
 		String nome;
-		Carta hand[];//Carta que o jogador tem é mão
-		Ficha fichas[];//Fichas que o jogador tem
+		private List<Carta> hand = new ArrayList<Carta>();//Carta que o jogador tem é mão
+		private List<Ficha> fichas = new ArrayList<Ficha>();//Fichas que o jogador tem
 		
 		
 		public Jogador(String n) {
@@ -11,45 +12,44 @@ class Jogador {
 		}
 		
 		
-		private void startFicha () {
+		private void startFicha () {//Ficha para serem recebidas no inicio do jogo
 			int i,j;
 			j = 0;
 			for (i=0;i<2;i++) {//Duas fichas de $100
-				fichas[j] = new Ficha(100);
+				fichas.add(new Ficha(100));
 				j++;
 			}
 			
 			for (i=0;i<2;i++) { //Duas fichas de $50
-				fichas[j] = new Ficha(50);
+				fichas.add(new Ficha(50));
 				j++;
 			}
 			
 			for (i=0;i<5;i++){//Cinco fichas de $20
-				fichas[j] = new Ficha(20);
+				fichas.add(new Ficha(20));
 				j++;
 			}
 			
 			for (i=0;i<5;i++){//Cinco fichas de $10
-				fichas[j] = new Ficha(10);
+				fichas.add(new Ficha(10));
 				j++;
 			}
 			
-			for (i=0;i<8;i++){
-				fichas[j] = new Ficha(5);//Oito fichas de $5
+			for (i=0;i<8;i++){//Oito fichas de $5
+				fichas.add(new Ficha(5));
 				j++;
 			}
 			
-			for (i=0;i<10;i++){
-				fichas[j] = new Ficha(1);//Dez fichas de $1
+			for (i=0;i<10;i++){//Dez fichas de $1
+				fichas.add(new Ficha(1));
 				j++;
 			}
 			return;
 		} 
 		
 		public void dealCarta(Carta novaCarta){//Da uma carta para a mão do jogador
-			int tam = hand.length;
-			
-			hand[tam] = novaCarta;
+			int tam = hand.size();
+			hand.add(novaCarta);
 			return;
 		}
 }
