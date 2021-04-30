aa8f4acd5cae7f4e320ebe570f6d3d9d43301d47
tmsantanna <71285512+tmsantanna@users.noreply.github.com>
2021-04-30 17:41:06 -0300

    Update dealer.java

--- dealer.java
+++ dealer.java
@@ -1,9 +1,9 @@
-import java.util.Random;
+import java.util.*;
 
 class Dealer {
 	//Dealer sera responsavel por ter o baralho, colocar as cartas na mesa, embaralhar as cartas e assim em diante
-	private Carta baralho[];//Cartas no baralho 
-	private Carta mesa[];//Cartas na mesa
+	private List<Carta> baralho = new ArrayList<Carta>();//Cartas no baralho 
+	private List<Carta> mesa =  new ArrayList<Carta>();//Cartas na mesa
 	
 	
 	private void createBaralho() {//Cria os 4 baralhos
@@ -16,12 +16,12 @@ class Dealer {
 			naipe = 1;
 			for(j=0;j<54;j++) {
 				Carta novaCarta = new Carta(num,naipe);//Cria uma nova Carta
-				baralho[i*54+j] = novaCarta;//Coloca a nova carta no baralho
+				baralho.add(novaCarta);//Coloca a nova carta no baralho
 				
 				num++;//Aumenta a carta
 				if (num == 14) //Se completou o naipe, passa par o proximo
 				{
-					num = 1;
+					num = 1;//Reinicia o valor das Cartas
 					naipe++;
 				}
 				
@@ -34,18 +34,16 @@ class Dealer {
 		int randomInt;
 		Random rand = new Random();
 		Carta temp;
-		int tam = baralho.length;//Tamanho do baralho
+		int tam = baralho.size();//Tamanho do baralho
 		
 		
 		for(i=0;i<3*tam;i++) {//Percorre o baralho 3 vezes, mudando posições de cada carta
 			randomInt = rand.nextInt(tam);//Pega um numero de 0 a tam-1
-			temp = baralho[randomInt];//Pega a Carta do baralho
-			
-			baralho[randomInt] = baralho[i];//Pega a carta e coloca no numero
-			baralho[i] = temp;//Completa a troca de lugar
+			temp = baralho.get(tam);//Pega a Carta do baralho
+			baralho.remove(tam);//Remove a carta da posição
+			baralho.add(temp);//Adiciona a carta novamente no final
 		}
 	}
-	
 	
 	
 	public Dealer() {
